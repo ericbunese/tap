73c65dc9786ff5a222688a7b36d58eb577a98877
ericbunese <ericbunese@gmail.com>
2016-11-16 22:24:14 -0200

    teste

--- DesktopPane.java
+++ DesktopPane.java
@@ -1,11 +1,11 @@
 import java.awt.*;
 import javax.swing.*;
 
-public class DesktopPane extends javax.swing.JDesktopPane
+public class DesktopPane extends JDesktopPane
 {
   private ImageIcon icon = new ImageIcon("backgroundImage.jpg");
   private Image image = icon.getImage();
-  private Image newimage = image.getScaledInstance(1024, 768, Image.SCALE_SMOOTH);
+  private Image newImage = image.getScaledInstance(1024, 768, Image.SCALE_SMOOTH);
 
   public DesktopPane()
   {
@@ -14,7 +14,7 @@ public class DesktopPane extends javax.swing.JDesktopPane
   @Override
   protected void paintComponent(Graphics g)
   {
-      super.paintComponent(g);
-      g.drawImage(newimage, 0, 0, this);
+    super.paintComponent(g);
+    g.drawImage(newImage, 0, 0, this);
   }
 }
